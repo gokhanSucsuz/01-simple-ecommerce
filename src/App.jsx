e98fffd5514bc4b3abc17fc5f7265d638ecb1792
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-04-21 21:19:43 +0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar"
 import PageContainer from "./containers/PageContainer"
 import Detail from "./pages/Detail"
 import Cart from "./pages/Cart"
+import Search from "./pages/Search"
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/products/:id" element={<Detail />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/search/:search" element={<Search />} />
         </Routes>
       </PageContainer>
 
