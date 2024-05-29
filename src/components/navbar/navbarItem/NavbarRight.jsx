10e1eb6298b6ac2d83f326675dd002930fe0595e
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-05-30 00:41:43 +0300

    ....

--- src/components/navbar/navbarItem/NavbarRight.jsx
+++ src/components/navbar/navbarItem/NavbarRight.jsx
@@ -26,8 +26,8 @@ const NavbarRight = () => {
       <div className="flex gap-4 md:gap-8 pe-5">
 
         <div className="relative cursor-pointer" onClick={() => navigate("wishlist")}>
-          <div className="absolute -top-3 -right-3 bg-red-400 text-white rounded-full w-4 h-4 flex items-center justify-center">{wishlist?.length}</div>
-          <AiOutlineHeart className="text-white" size={20} />
+          <div className="absolute -top-3 -right-3 bg-red-400 text-white w-4 h-4 flex items-center justify-center">{wishlist?.length}</div>
+          <AiOutlineHeart className="text-white hover:bg-red-700 rounded-full hover:p-1" size={20} />
         </div>
         <div className="relative cursor-pointer" onClick={() => navigate("cart")}>
           <div className="absolute -top-3 -right-3 bg-red-400 text-white rounded-full w-4 h-4 flex items-center justify-center">{carts?.length}</div>
